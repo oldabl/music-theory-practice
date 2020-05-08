dd0ab535d70c3dee60030df03b6ce3c9b9ad63c3
Olivier Brunel <olivierdabl@oliviers-mbp.home>
2020-05-08 02:27:05 +0200

    Improve game entry

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 
 from Games import NoteDifferences
 from Games import NotesOnThePiano
+from Games import ChordNumberOfScale
+
+from Games import Helpers
+
+import sys, os
 
 if __name__ == "__main__":
-  print("1. Note differences")
-  print("2. Notes on the piano")
-  choice = input("Which game do we play? ")
-  if choice == "1":
-    game = NoteDifferences.NoteDifferences()
-    game.play()
-  elif choice == "2":
-    game = NotesOnThePiano.NotesOnThePiano()
-    game.play()
-  else:
-    print("No game chosen, will quit")
+  argv = sys.argv[1:]
+  notetype = "traditional"
+  if len(argv) > 0:
+    notetype = argv[0]
+    if notetype != "traditional" and notetype != "alphabet":
+      print("Note type must be 'traditional' or 'alphabet'")
+      sys.exit(0)
+    os.environ["MOZARTWILLIAMS"] = notetype
+
+  Helpers.clearTerminal()
+  choice = ""
+  while choice != "q":
+    Helpers.cleanLinesAhead(6)
+    print()
+    print("1. Note differences")
+    print("2. Notes on the piano")
+    print("3. Chord number to scale")
+    print()
+
+    choice = input("Which game do we play? ")
+
+    if choice == "1":
+      game = NoteDifferences.NoteDifferences()
+      game.play()
+    elif choice == "2":
+      game = NotesOnThePiano.NotesOnThePiano()
+      game.play()
+    elif choice == "3":
+      game = ChordNumberOfScale.ChordNumberOfScale()
+      game.play()
+
+  # After the lopp
+  Helpers.printThanks()
