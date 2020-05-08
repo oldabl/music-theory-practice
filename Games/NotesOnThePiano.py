9b4283c101e79cf2b323c649b1b29e04168ad266
Olivier Brunel <olivierdabl@oliviers-mbp.home>
2020-05-08 15:42:12 +0200

    New piano print

--- Games/NotesOnThePiano.py
+++ Games/NotesOnThePiano.py
@@ -2,26 +2,26 @@
 import random, os, sys
 import Games.Helpers as Helpers
 
-PIANO_ASCII = """
-|  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
-|  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
-|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |  |_| |_| |_|  |
-|   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-|___|___|___|___|___|___|___|___|___|___|___|___|___|___|"""
-PIANO_WIDTH = 57
+PIANO_ASCII = {
+  1: "|   █   █   |   █   █   █   |   █   █   |   █   █   █   |",
+  2: "|   █   █   |   █   █   █   |   █   █   |   █   █   █   |",
+  3: "|   █   █   |   █   █   █   |   █   █   |   █   █   █   |",
+  4: "|   |   |   |   |   |   |   |   |   |   |   |   |   |   |",
+  5: "|___|___|___|___|___|___|___|___|___|___|___|___|___|___|",
+}
 NOTE_SPACE_POSITIONS = {
-   0:  2,
-   1:  4,
-   2:  6,
-   3:  8,
-   4: 10,
-   5: 14,
-   6: 16,
-   7: 18,
-   8: 20,
-   9: 22,
-  10: 24,
-  11: 26,
+   0: [(4, 5, 1, 4),(1, 3, 1, 3)],
+   1: [(1, 3, 4, 5)],
+   2: [(4, 5, 5, 8),(1, 3, 6, 7)],
+   3: [(1, 3, 8, 9)],
+   4: [(4, 5, 9, 12),(1, 3, 10, 12)],
+   5: [(4, 5, 13, 16),(1, 3, 13, 15)],
+   6: [(1, 3, 16, 17)],
+   7: [(4, 5, 17, 20),(1, 3, 18, 19)],
+   8: [(1, 3, 20, 21)],
+   9: [(4, 5, 21, 24),(1, 3, 22, 23)],
+  10: [(1, 3, 24, 25)],
+  11: [(4, 5, 25, 28),(1, 3, 26, 28)],
 }
 NOTE2_OFFSET = 28
 
@@ -29,33 +29,53 @@ class NotesOnThePiano(Helpers.Game):
   def __init__(self):
     super().__init__()
     print("\nWelcome to notes on the piano game!\n")
-    self.addExtraPositions()
-  
-  def addExtraPositions(self):
-    self.notePositions = dict()
-    for (key, value) in NOTE_SPACE_POSITIONS.items():
-      self.notePositions[key] = value
-      self.notePositions[key+len(NOTE_SPACE_POSITIONS)] = value+NOTE2_OFFSET
+    self.notepositions = NOTE_SPACE_POSITIONS
+
+
+  def printPianoAndNoteSelected(self, notenumber, secondposition, goback):
+    if self.notedictionary.isSharp(notenumber):
+      pinnotechar = '▒'
+    else:
+      pinnotechar = '░'
+
+    if goback:
+      Helpers.goBackUpLines(5)
+
+    # Print piano with note played
+    for (line, value) in PIANO_ASCII.items():
+      # Find right coordinates for current line
+      (notecoordinatesys, notecoordinatesye, notecoordinatesxs, notecoordinatesxe) = (-1,-1,-1,-1)
+      for xy in self.notepositions[notenumber]:
+        if xy[0] <= line <= xy[1]:
+          (notecoordinatesys, notecoordinatesye, notecoordinatesxs, notecoordinatesxe) = xy
+      for position in range(len(value)):
+        # Print at right place
+        if notecoordinatesxs <= position < notecoordinatesxe:
+          sys.stdout.write(pinnotechar)
+        else:
+          sys.stdout.write(value[position])
+      sys.stdout.write("\n")
+      sys.stdout.flush()
+
   
   def play(self):
-    print(PIANO_ASCII)
-
     answer = ""
+    firstround = True
 
     while answer != "q":
+      # Select note number (difficulty handling for sharps to appear)
       notenumber = self.notedictionary.getRandomNoteNumber(includesharp=False)
-      if self.score > 12:
+      if self.score >= 12:
         notenumber = self.notedictionary.getRandomNoteNumber()
 
+      # Will print piano with note chosen
       notesecondposition = random.randint(0, 1)
-
-      correctanswernoteposition = self.notePositions[notenumber] + notesecondposition * NOTE2_OFFSET
-      correctnotename = self.notedictionary.getNoteName(notenumber)
-
-      pinnotechar = '^'
-
+      self.printPianoAndNoteSelected(notenumber, (notesecondposition==1), (firstround==False))
       Helpers.cleanLinesAhead(1)
-      print(correctanswernoteposition*" "+pinnotechar)
+      print()
+
+      # Deduct correct answer
+      correctnotename = self.notedictionary.getNoteName(notenumber)
 
       # Let player play
       Helpers.cleanLinesAhead(1)
@@ -70,6 +90,7 @@ class NotesOnThePiano(Helpers.Game):
         print("WRONG! Actually it was a "+correctnotename)
       
       Helpers.goBackUpLines(3)
+      firstround = False
     
     # After loop
     self.printBestScore()
